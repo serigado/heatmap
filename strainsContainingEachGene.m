1f0a4c3baffe6a7c8dd0d27bf15bb7788db31f33
serigado <serigado@gmail.com>
2015-07-01 21:37:25 +0100

    strainsContainingEachGene supports group_strains like C, W,

--- strainsContainingEachGene.m
+++ strainsContainingEachGene.m
@@ -1,55 +1,36 @@
-% 02-Jun-2015 00:20
-
 % print to file stats_gene_all the percentages of occurrence of a gene in a strain group
 
-
-
 % insert here any strains to exclude (cell array of strings)
-strains_to_exclude = {'A-208185', 'A-216185'};
+strains_to_exclude = {};
 
 % insert strain groups to analyse (order matters). Must be cell array
 % can choose between 'avian', 'canine', 'bovine', 'human'
-strain_groups = {'canine', 'avian'};
-
+strain_groups = {'C', 'W'};
 
 %% import data
-table = import_data('work_matrix');
-
+table = import_data('pan_matrix_70284');
 
 %% select data
 % select desired group strains and remove unwanted strains
 % it will change the order of the strains!
 sub_table = select_data(table, strain_groups, strains_to_exclude);
-clear table strain_groups strains_to_exclude
-
-
-
 
 %% iterate through each gene and calculate the percentagem of occurrence in each strain group
-
 % data are saved to a file
 fid = fopen('stats_gene_all', 'W');
 
+selectStrain = @(firstLetter, names)cellfun(@(x)x(1)==firstLetter, names, 'UniformOutput', true);
+
 for gene = 1:sub_table.nr_genes
-    
-    % calculate percentages for each of the strain groups
     strainsContainingGene = sub_table.data(:, gene)';
-
-    percentage_in_avian = 100*sum(strainsContainingGene & sub_table.avianStrain) / sum(sub_table.avianStrain);
-    percentage_in_canine = 100*sum(strainsContainingGene & sub_table.canineStrain) / sum(sub_table.canineStrain);
-    percentage_in_bovine = 100*sum(strainsContainingGene & sub_table.bovineStrain) / sum(sub_table.bovineStrain);
-    percentage_in_human = 100*sum(strainsContainingGene & sub_table.humanStrain) / sum(sub_table.humanStrain);
-
-    fprintf(fid, '%s,%2.1f,%2.1f,%2.1f,%2.1f\n', ...
-        sub_table.genes_names{gene}, ...
-        percentage_in_avian, ...
-        percentage_in_canine, ...
-        percentage_in_bovine, ...
-        percentage_in_human);
-
+    fprintf(fid, '%s', sub_table.genes_names{gene});
+    % calculate percentages for each of the strain groups
+    for s=1:numel(strain_groups)
+        sel_strain = selectStrain(strain_groups{s}, sub_table.strains_names);
+        percentage = 100*sum(strainsContainingGene & sel_strain) / sum(sel_strain);
+        fprintf(fid, ',%1.2f', percentage);
+    end
+    fprintf(fid, '\n');
 end
 
 fclose(fid);
-
-
-
